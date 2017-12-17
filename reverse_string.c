cff04dd2cb4d9952157ebb3a3cb1d4e13ef364a7
jo-qzy <2651933495@qq.com>
2017-12-17 22:59:00 +0800

    给定一个逆序的句子，单词为顺序，要求正序输出

--- reverse_string.c
+++ reverse_string.c
@@ -7,30 +7,45 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <ctype.h>//为了用isspace函数，is space，是空格
 
-char * ReverseString(char arr[]);
+char * Search(char arr[]);
+void ReverseString(char *head, char *tail);
 
 int main()
 {
-	char arr[] = "I am a stdudent.";
+	char arr[] = "stdudent a am I";//原始字符串
 	printf("原字符串:%s\n", arr);
-	printf("逆转后:%s\n", ReverseString(arr));
+	printf("逆转后:%s\n", Search(arr));
 	system("pause");
 	return 0;
 }
 
-char * ReverseString(char arr[])
+char * Search(char arr[])
 {
-	char *head,*tail;
-	char tmp = 0;
+	assert(arr != '\0');
+	char *head, *tail;
 	head = arr;
 	tail = arr;
 	while (*tail != '\0')
 	{
+		if (isspace(*tail))//先逆转每个单词
+		{
+			ReverseString(head, tail - 1);
+			head = tail + 1;//头指针要指向单词首位
+		}
 		tail++;
 	}
-	tail--;
-	while (tail - head > 0)
+	ReverseString(arr, tail - 1);//逆转整个句子
+	return arr;
+}
+
+void ReverseString(char *head, char *tail)
+{
+	char tmp = 0;
+	while (tail - head > 0)//真正的交换操作
 	{
 		tmp = *tail;
 		*tail = *head;
@@ -38,5 +53,4 @@ char * ReverseString(char arr[])
 		head++;
 		tail--;
 	}
-	return arr;
 }
