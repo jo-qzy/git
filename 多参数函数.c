e44d9df5b3f250fb516609f8cdf875fa23ac8ec3
jo-qzy <2651933495@qq.com>
2017-12-26 22:20:16 +0800

    几个多参数函数的实现，完善了模拟简单printf函数

--- 多参数函数.c
+++ 多参数函数.c
@@ -1,19 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+//1.使用可变参数，实现函数，求函数参数的平均值。
+//2.使用可变参数，实现函数，求函数参数的最大值。
+//3.模拟实现printf函数，可完成下面的功能:
+//print("s ccc d.\n","hello",'b','i','t'，100); 
+
 #include <stdio.h>
 #include <stdarg.h>
+
+
 
 int Avg(int n, ...)
 {
 	int i = 0;
 	int sum = 0;
-	va_list num;
-	va_start(num, n);//初始化指针位置，指针在第一个变量的地址的后一个变量的初始地址
+	va_list ptr;//定义一个char*的指针ptr
+	va_start(ptr, n);//初始化指针位置，指针指向参数列表第一个参数也就是n
 	for (i = 0; i < n;i++)
 	{
-		sum = sum + va_arg(num,int);
+		sum = sum + va_arg(ptr,int);//获取下一个数据
 	}
-	va_end(num);
+	va_end(ptr);
 	return sum / n;
 }
 
@@ -21,25 +28,72 @@ int Max(int n, ...)
 {
 	int max = 0;
 	int i = 0;
-	va_list num;
-	va_start(num, n);
-	max = va_arg(num, int);
+	va_list ptr;
+	va_start(ptr, n);
+	max = va_arg(ptr, int);
 	for (i = 1; i < n; i++)
 	{
-		int tmp = va_arg(num, int);
+		int tmp = va_arg(ptr, int);
 		if (max < tmp)
 			max = tmp;
 	}
+	va_end(ptr);
 	return max;
 }
 
-
+int print(char *p, ...)
+{
+	char *q = NULL;
+	int count = 0;
+	va_list cha;
+	va_start(cha, p);
+	while (*p != '\0')
+	{
+		if (*p == '%')
+		{
+			p++;
+			switch (*p)
+			{
+			case 's'://输出字符串
+				q = va_arg(cha, char *);
+				while (*q != '\0')
+				{
+					putchar(*q);
+					q++;
+					count++;
+				}
+				break;
+			case 'c'://输出字符
+				putchar(va_arg(cha, char));
+				count++;
+				break;
+			case 'd':
+				printf("%d",va_arg(cha, int));//惭愧，这里不知道怎么输出一个数字
+				break;//如果你看到了并向到解决方法，可以私我
+			case ' '://空格
+				putchar(' ');
+				break;
+			case '\n'://换行
+				putchar('\n');
+				break;
+			default:
+				break;
+			}
+		}
+		else
+			putchar(*p);
+		p++;
+	}
+	va_end(cha);
+	return count;
+}
 
 int main()
 {
 	int n = 6;
 	printf("平均值函数：%d\n", Avg(n, 5, 7, 8, 9, 10, 3));
 	printf("最大值函数：%d\n", Max(n, 5, 7, 8, 9, 10, 3));
+	print("字符串输出测试：字符串：%s\n		字符：%c%c%c\n		数字：%d\n", "hello", 'b', 'i', 't',100);
 	system("pause");
 	return 0;
 }
