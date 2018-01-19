fcfde47593757ffb9ca5f23301099a78a6a5cf6c
jo-qzy <2651933495@qq.com>
2018-01-19 16:26:30 +0800

    增加了位运算查找不同数字的方法

--- find_diff_num2.c
+++ find_diff_num2.c
@@ -7,7 +7,8 @@
 #include <windows.h>
 
 void sort(int arr[], int size);
-void find_diff(int arr[], int size);
+void find_diff1(int arr[], int size);
+void find_diff2(int arr[], int size);
 void print_arr(int arr[], int size);
 
 int main()
@@ -17,10 +18,14 @@ int main()
 	int size = sizeof(arr) / sizeof(arr[0]);
 	printf("原数组：");
 	print_arr(arr, size);
-	sort(arr,size);
+	sort(arr, size);
+	printf("位运算实现：\n");
+	find_diff1(arr, size);
+	printf("排序实现：\n");
+	
 	printf("新数组：");
 	print_arr(arr, size);
-	find_diff(arr, size);
+	find_diff2(arr, size);
 	system("pause");
 	return 0;
 }
@@ -35,6 +40,34 @@ void print_arr(int arr[], int size)
 	printf("\n");
 }
 
+//思路1：位运算法
+void find_diff1(int arr[], int size)
+{
+	int flag = 0;//flag作为分类两个组的依据
+	int i = 0;
+	int data1 = 0, data2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		flag ^= arr[i];
+	}
+	i = 0;
+	while (flag & 1 != 1)
+	{
+		flag = flag >> 1;
+		i++;
+	}
+	flag = 1 << i;
+	for (i = 0; i < size; i++)
+	{
+		if ((flag ^ arr[i]) & 1 == 1)
+			data1 ^= arr[i];
+		else
+			data2 ^= arr[i];
+	}
+	printf("%d %d\n", data1, data2);
+}
+
+//思路2：排序法
 void sort(int arr[], int size)
 {
 	int i = 0;
@@ -53,7 +86,7 @@ void sort(int arr[], int size)
 	}
 }
 
-void find_diff(int arr[], int size)
+void find_diff2(int arr[], int size)
 {
 	printf("不同的数:");
 	int i = 0;
