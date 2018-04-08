81cf26394523bb4ec1277c377e2763b2b1f57f79
jo-qzy <2651933495@qq.com>
2018-04-08 22:09:31 +0800

    修改了队列的大量错误。。。加入了队列面试题

--- 队列.h
+++ 队列.h
@@ -19,7 +19,7 @@ typedef struct Queue
 	QueueNode* _tail;
 }Queue;
 
-void QueueInit(Queue* q);
+Queue* QueueInit();
 void QueuePush(Queue* q, DataType x);
 void QueuePop(Queue* q);
 DataType QueueFront(Queue* q);
@@ -34,18 +34,29 @@ QueueNode* BuyNewNode()
 	return newnode;
 }
 
-void QueueInit(Queue* q)
+Queue* QueueInit()
 {
-	q->_head = BuyNewNode();
+	Queue* q = (Queue*)malloc(sizeof(Queue));
+	q->_head = NULL;
 	q->_tail = q->_head;
+	return q;
 }
 
 void QueuePush(Queue* q, DataType x)
 {
 	assert(q);
+	if (q->_tail == NULL)
+	{
+		q->_tail = BuyNewNode();
+		q->_head = q->_tail;
+	}
+	else
+	{
+		q->_tail->_next = BuyNewNode();
+		q->_tail = q->_tail->_next;
+	}
 	q->_tail->_data = x;
-	q->_tail = q->_tail->_next;
-	q->_tail = BuyNewNode();
+	q->_tail->_next = NULL;
 }
 
 void QueuePop(Queue* q)
@@ -76,7 +87,11 @@ size_t QueueSize(Queue* q)
 {
 	assert(q);
 	QueueNode* cur = q->_head;
-	size_t count = 0;
+	size_t count = 1;
+	if (cur == NULL)
+	{
+		return 0;
+	}
 	while (cur != q->_tail)
 	{
 		cur = cur->_next;
@@ -87,11 +102,32 @@ size_t QueueSize(Queue* q)
 
 int QueueEmpty(Queue* q)
 {
-	if (q->_head == q->_tail)
+	if (q->_head == NULL)
 	{
 		return 0;
 	}
 	return 1;
+}
+
+void TestQueue()//队列测试用例
+{
+	Queue* q = QueueInit();
+	QueuePush(q, 1);
+	QueuePush(q, 2);
+	QueuePush(q, 3);
+	QueuePush(q, 4);
+	printf("%d ", QueueSize(q));
+	printf("%d ", q->_head->_data);
+	QueuePop(q);
+	printf("%d ", QueueSize(q));
+	printf("%d ", q->_head->_data);
+	printf("%d ", QueueEmpty(q));
+	QueuePop(q);
+	printf("%d ", QueueEmpty(q));
+	QueuePop(q); 
+	printf("%d ", QueueEmpty(q));
+	QueuePop(q);
+	printf("%d ", QueueEmpty(q));
 }
 
 //面试题
@@ -102,16 +138,11 @@ typedef struct QueueStack
 	Queue* _q2;
 }QueueStack;
 
-QueueStack* Stack_QueueInit();
-void StackPush_Queue(QueueStack* qs, DataType data);
-void StackPop_Queue(QueueStack* qs);
-DataType StackTop_Queue(QueueStack* qs);
-
 QueueStack* Stack_QueueInit()
 {
 	QueueStack* new_qs = (QueueStack*)malloc(sizeof(QueueStack));
-	QueueInit(new_qs->_q1);
-	QueueInit(new_qs->_q2);
+	new_qs->_q1 = QueueInit();
+	new_qs->_q2 = QueueInit();
 	return new_qs;
 }
 
@@ -165,4 +196,19 @@ DataType StackTop_Queue(QueueStack* qs)
 	{
 		return QueueBack(qs->_q1);
 	}
+}
+
+void Test2QToS()
+{
+	QueueStack* qs = Stack_QueueInit();
+	StackPush_Queue(qs, 1);
+	StackPush_Queue(qs, 2);
+	StackPush_Queue(qs, 3);
+	StackPush_Queue(qs, 4);
+	StackPush_Queue(qs, 5);
+	printf("%d ", StackTop_Queue(qs));
+	StackPop_Queue(qs);
+	printf("%d ", StackTop_Queue(qs));
+	StackPop_Queue(qs);
+	printf("%d ", StackTop_Queue(qs));
 }
