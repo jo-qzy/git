09e3476b72a870ef1f7b7ecb634561e83daabe8c
jo-qzy <2651933495@qq.com>
2018-04-09 23:28:02 +0800

    增加了共享栈的的题目

--- 栈.h
+++ 栈.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 typedef int DataType;
-#define size 10
+#define size 4
 
 typedef struct Stack
 {
@@ -21,6 +21,7 @@ void StackPop(Stack* s);
 DataType StackTop(Stack* s);
 size_t StackSize(Stack* s);
 int StackEmpty(Stack* s);
+void StackDestroy(Stack** s);
 
 Stack* StackInit()
 {
@@ -70,6 +71,11 @@ int StackEmpty(Stack* s)
 	return 1;
 }
 
+void StackDestroy(Stack** s)
+{
+	free((*s)->_array);
+	free((*s));
+}
 
 //面试题
 //1.两个栈实现一个队列
@@ -198,4 +204,162 @@ void TestMinStack()//测试用例
 	MinStackPop(ms);
 	MinStackPop(ms);
 	printf("%d\n", MinStackMin(ms));
+}
+
+//3.一个数组实现共享栈
+typedef struct UnionStack
+{
+	DataType* _array;
+	size_t _top1;
+	size_t _top2;
+	size_t	_capacity;
+}UStack;
+
+UStack* UStackInit();
+void UStackPush(UStack* us, size_t flag, DataType data);
+void UStackPop(UStack* us, size_t flag);
+DataType UStackTop(UStack* us, size_t flag);
+size_t UStackSize(UStack* us, size_t flag);
+int UStackEmpty(UStack* us, size_t flag);
+
+UStack* UStackInit()
+{
+	UStack* new_ustack = (UStack*)malloc(sizeof(UStack));
+	new_ustack->_capacity = size;
+	new_ustack->_array = (DataType*)malloc(sizeof(DataType) * new_ustack->_capacity);
+	new_ustack->_top1 = 0;
+	new_ustack->_top2 = 0;
+	return new_ustack;
+}
+
+void UStackPush(UStack* us, size_t flag, DataType data)
+{
+	assert(us);
+	//栈选择检查，防御性编程
+	if ((flag != 1) && (flag != 2))
+	{
+		printf("选择栈错误！\n");
+		assert(0);
+	}
+	//压栈前容量检查，容量不够重新申请
+	if ((us->_top1 == us->_capacity / 2 - 1) || (us->_top2 == us->_capacity / 2 - 1))
+	{
+		us->_capacity *= 2;
+		DataType* new_us = (DataType*)malloc(sizeof(DataType) * us->_capacity);
+		memcpy(new_us, us->_array, sizeof(DataType) * us->_top1);
+		memcpy(new_us + us->_capacity / 2, us->_array + us->_capacity / 4, sizeof(DataType) * us->_top2);
+		free(us->_array);
+		us->_array = new_us;
+	}
+	//选择栈进行操作
+	if (flag == 1)
+	{
+		*(us->_array + us->_top1) = data;
+		us->_top1++;
+	}
+	else if (flag == 2)
+	{
+		*(us->_array + us->_capacity / 2 + us->_top2) = data;
+		us->_top2++;
+	}
+}
+
+void UStackPop(UStack* us, size_t flag)
+{
+	assert(us);
+	if ((flag != 1) && (flag != 2))
+	{
+		printf("选择栈错误！\n");
+		assert(0);
+	}
+	if (flag == 1)
+	{
+		us->_top1--;
+	}
+	else if (flag == 2)
+	{
+		us->_top2--;
+	}
+}
+
+DataType UStackTop(UStack* us, size_t flag)
+{
+	assert(us);
+	if ((flag != 1) && (flag != 2))
+	{
+		printf("选择栈错误！\n");
+		assert(0);
+	}
+	if (flag == 1)
+	{
+		return *(us->_array + us->_top1 - 1);
+	}
+	else if (flag == 2)
+	{
+		return *(us->_array + us->_capacity / 2 + us->_top2 - 1);
+	}
+}
+
+size_t UStackSize(UStack* us, size_t flag)
+{
+	assert(us);
+	if ((flag != 1) && (flag != 2))
+	{
+		printf("选择栈错误！\n");
+		assert(0);
+	}
+	if (flag == 1)
+	{
+		return *(us->_array + us->_top1 - 1);
+	}
+	else if (flag == 2)
+	{
+		return *(us->_array + us->_capacity / 2 + us->_top2 - 1);
+	}
+}
+
+int UStackEmpty(UStack* us, size_t flag)
+{
+	assert(us);
+	if ((flag != 1) && (flag != 2))
+	{
+		printf("选择栈错误！\n");
+		assert(0);
+	}
+	if (flag == 1)
+	{
+		if (us->_top1 == 0)
+		{
+			return 0;
+		}
+	}
+	else if (flag == 2)
+	{
+		if (us->_top2 == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void TestUnionStack()
+{
+	UStack* us = UStackInit();
+	UStackPush(us, 1, 4);
+	UStackPush(us, 1, 3);
+	UStackPush(us, 1, 2);
+	UStackPush(us, 1, 1);
+	UStackPush(us, 2, 1);
+	UStackPush(us, 2, 2);
+	UStackPush(us, 2, 3);
+	UStackPush(us, 2, 4);
+	printf("%d\n", UStackTop(us, 1));
+	printf("%d\n", UStackTop(us, 2));
+	UStackPop(us, 1);
+	UStackPop(us, 2);
+	printf("%d\n", UStackTop(us, 1));
+	printf("%d\n", UStackTop(us, 2));
+	printf("栈1容量：%d\n", UStackSize(us, 1));
+	printf("栈2容量：%d\n", UStackSize(us, 2));
 }
