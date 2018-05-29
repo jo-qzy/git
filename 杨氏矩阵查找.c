82b7cc4f9a0ea49933fd5fc1074f63d6a4615900
jo-qzy <2651933495@qq.com>
2018-05-29 23:06:43 +0800

    杨氏矩阵

--- 杨氏矩阵查找.c
+++ 杨氏矩阵查找.c
@@ -12,20 +12,20 @@ int main()
 {
 	int arr[3][3] = { 1,3,4,2,4,5,4,5,6 };
 	int i = 0;
-	int size = sizeof(arr)/sizeof(arr[0]);
+	int size = sizeof(arr[0])/sizeof(arr[0][0]);
 	int num = 10;
 	int j = size - 1;
 	while(i < size && i >= 0 && j < size && j >= 0)
 	{
-		if (arr[i][j] > num)
+		if (arr[i][j] < num)
 		{
 			i++;
 		}
-		if (arr[i][j] < num)
+		else if (arr[i][j] > num)
 		{
 			j--;
 		}
-		if (arr[i][j] == num)
+		else if (arr[i][j] == num)
 		{
 			printf("find it!\n");
 			system("pause");
